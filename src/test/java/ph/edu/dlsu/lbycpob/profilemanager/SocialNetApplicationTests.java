f5c059084f5d521b396a979fbbb16d5274368e51
kkkk <miguel_cristobal@dlsu.edu.ph>
2026-07-21 12:35:47 +0800

    Setup

---
+++ src/test/java/ph/edu/dlsu/lbycpob/profilemanager/SocialNetApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.socialnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
